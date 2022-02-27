cce4a4215e1dddc8d473453c92636a3eae26f558
m-iida555 <user@usernoMacBook-Pro.local>
2022-02-27 16:39:10 +0900

    static_pages_2 ①featureテストの追加②header内のデザインを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = 'SCENES撮影会'
+    if page_title.blank?
+      base_title
+    else
+      "#{page_title} - #{base_title}"
+    end
+  end
 end
---
+++ spec/features/static_page_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.feature "StaticPages", type: :feature do
+  describe "トップページ"  do
+    background do
+      visit root_path
+    end
+
+    scenario "タイトルの文字が表示されているかどうか" do
+      expect(page).to have_content "SCENES撮影会"
+    end
+
+    scenario "タイトルの文字が表示されているかどうか" do
+      expect(page).to have_title full_title
+    end
+  end
+
+  describe "ABOUT US" do
+    background do
+      visit about_path
+    end
+    
+    scenario "ABOUT USの文字列が表示されているかどうか" do
+      expect(page).to have_content "ABOUT US"
+    end
+
+    scenario "タイトルの文字が表示されているかどうか" do
+      expect(page).to have_title full_title('ABOUT US')
+    end
+  end
+
+  describe "利用規約" do
+    background do
+      visit use_of_terms_path
+    end
+
+    scenario "利用規約の文字列が表示されているかどうか" do
+      expect(page).to have_content "利用規約"
+    end
+
+    scenario "タイトルの文字が表示されているかどうか" do
+      expect(page).to have_title full_title('利用規約')
+    end
+  end
+end
